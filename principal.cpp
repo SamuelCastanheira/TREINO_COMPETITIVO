e520054880e39240dcd3364e590f4f20d5713539
Samuel Castanheira <96214953+SamuelCastanheira@users.noreply.github.com>
2023-10-09 06:32:33 -0300

    Update principal.cpp

--- principal.cpp
+++ principal.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main(void){
-  
+  string palavra;
+  cout << "Digite a string desejada: " << endl;
+  cin>>palavra;
+  cout << lis(palavra <<endl;
+  return 0;
 }
